f89f9a4fe430b15c99afb1b594d1ff6ea360d5f8
Pavlov Artem <inc82319@gmail.com>
2026-02-17 16:30:06 +0300

    Добавлена функция обновления коллекции через api

--- backend/rag_engine/vector_schemes.py
+++ backend/rag_engine/vector_schemes.py
@@ -6,25 +6,27 @@ from pydantic import BaseModel, Field
 
 
 class VectorDbScheme(BaseModel):
-    vector_database: str = Field(...,
-                                       example='collection_name',
-                                       description='Список заполняемых коллекций'
-                                       )
+    vector_database: str = Field(
+        ...,
+        example='collection_name',
+        description='Список заполняемых коллекций'
+    )
 
 
 class FieldsDescScheme(BaseModel):
-    fields_description: dict[str, dict] = Field(example={'table1_name': {'field1': 'desc1', 'field2': 'desc2'},
-                                                         'table2_name': {'field1': 'desc1', 'field2': 'desc2'}
-                                                         })
+    fields_description: dict[str, dict] = Field(example={
+        'table1_name': {'field1': 'desc1', 'field2': 'desc2'},
+        'table2_name': {'field1': 'desc1', 'field2': 'desc2'}
+    })
+
 
 class PointResponseSchema(BaseModel):
     id: uuid.UUID = Field(...)
-    table_name: str = Field(...,)
+    table_name: str = Field(..., )
     value: dict = Field(...)
 
     @classmethod
     def format_point(cls, point):
-
         id = point.id
         payload = point.payload
 
@@ -36,14 +38,16 @@ class PointResponseSchema(BaseModel):
 
 
 class PointUpdateScheme(BaseModel):
-    id: uuid.UUID = Field(...,
-                          description='ID точки'
-                          )
-    table_name: str = Field(...,
-                            description='Название таблицы',
-                            example='table_name',
-                            )
-    value: dict = Field(..., description='Поля и описания', example={
-        'field1': 'desc1',
-        'field2': 'desc2'
-    })
+    id: uuid.UUID = Field(..., description='ID точки')
+    table_name: str = Field(
+        ...,
+        description='Название таблицы',
+        example='table_name',
+    )
+    value: dict = Field(
+        ...,
+        description='Поля и описания',
+        example={
+            'field1': 'desc1',
+            'field2': 'desc2'
+        })
